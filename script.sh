305d018ca5cfd5742f1b32b9d9fd753c5798f03d
mnotin <94868539+mnotin@users.noreply.github.com>
2024-06-20 17:10:48 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,7 +29,7 @@ sudo apt install -y postgresql
 sudo -u postgres createdb mercator
 
 # Configuration du mot de passe de l'utilisateur
-sudo -u postgres psql -c 'ALTER ROLE postgres PASSWORD 'postgres';'
+sudo -u postgres psql -c "ALTER ROLE postgres PASSWORD 'postgres';"
 
 # Création d'un fichier .env à la racine du répertoire du projet
 cd /var/www/mercator
