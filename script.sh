223040124bc1dc957076641085c9ee44de84d505
mnotin <94868539+mnotin@users.noreply.github.com>
2024-06-24 10:38:47 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@ sudo apt update && sudo apt full-upgrade
 # Installation de PHP et librairies PHP
 sudo apt install -y php-zip php-curl php-mbstring php-dom php-ldap php-soap php-xdebug php-mysql php-gd libapache2-mod-php php8.3-pgsql
 
-# Installation de Apache2, GIT, Graphviz et Composer
-sudo apt install -y apache2 git graphviz composer
+# Installation de Apache2, GIT, Graphviz, Composer et PostgreSQL
+sudo apt install -y apache2 git graphviz composer postgresql
 
 # Création du répertoire du projet
 cd /var/www
@@ -21,9 +21,6 @@ composer update
 
 # Publication de tous les assets publiables parmi les paquets "vendor"
 php artisan vendor:publish --all
-
-# Installation de PostgreSQL
-sudo apt install -y postgresql
 
 # Création de la base de données mercator
 sudo -u postgres createdb mercator
