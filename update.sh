de76b7260df143129204b9946a7cf607380cfd65
mnotin <94868539+mnotin@users.noreply.github.com>
2024-06-25 14:43:03 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,17 @@
+cd /var/www/mercator
+mkdir -p backup
 
+# Réalisation d'un backup de la base de données
+pg_dump mercator > backup/mercator_backup.sql
+
+# Récupèration des sources depuis Git
+git pull
+
+# Migration de la base de données
+php artisan migrate
+
+# Mise à jour des librairies
+composer update
+
+# Nettoyage des caches
+php artisan config:clear && php artisan view:clear
