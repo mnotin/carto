afff4d31fef142a095f25d2880fb7cf95f0b8289
mnotin <94868539+mnotin@users.noreply.github.com>
2024-08-23 11:28:50 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,6 +6,10 @@ mkdir -p backups_bd
 date_var=`date "+%Y-%m-%d_%H-%M-%S"`
 sudo -u postgres pg_dump mercator > backups_bd/mercator_backup_$date_var.sql
 
+# Extinction du serveur Apache2
+sudo systemctl stop apache2.service
+sudo chown -R $USER:$GROUP /var/www/mercator
+
 # Récupération des sources depuis Git
 cd /var/www/mercator
 git pull
@@ -18,3 +22,7 @@ composer update
 
 # Nettoyage des caches
 php artisan config:clear && php artisan view:clear
+
+# Lancement du serveur Apache2
+sudo chown -R www-data:www-data /var/www/mercator
+sudo systemctl start apache2.service
